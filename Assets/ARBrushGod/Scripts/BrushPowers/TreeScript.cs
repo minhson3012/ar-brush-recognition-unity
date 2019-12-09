e565db5a5f5213c84dc43d326e094ddcd48dd87c
donkriegs <sonnm30@gmail.com>
2019-12-09 16:56:28 +0700

    adding trees

---
+++ Assets/ARBrushGod/Scripts/BrushPowers/TreeScript.cs
@@ -0,0 +1,45 @@
+using GoogleARCore;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+namespace BrushGestures
+{
+    public class TreeScript : MonoBehaviour
+    {
+        public GameObject Tree;
+        public float speed = 0.5f;
+        public float duration = 1.5f;
+        GameObject power;
+        BrushPowers brushPowers;
+        GameObject dummy;
+        DummyScript dummyScript;
+        GameObject circle;
+        Vector3 minScale = new Vector3(0.001f, 0.001f, 0.001f);
+        Vector3 maxScale = new Vector3(0.006f, 0.006f, 0.006f);
+        float lerpTime = 0f;
+
+        public void SpawnTree()
+        {
+            // countdown = delay;
+            dummy = GameObject.FindGameObjectWithTag("Dummy");
+            dummyScript = dummy.GetComponent<DummyScript>();
+            brushPowers = GetComponent<BrushPowers>();
+            circle = GameObject.Find("Circle");
+            Vector3 position = new Vector3(circle.transform.position.x, circle.transform.position.y, circle.transform.position.z);
+            power = Instantiate(Tree, position, Tree.transform.rotation);
+            power.transform.parent = GameObject.Find("Anchor").transform;
+            brushPowers.CleanupUI();
+            // Destroy(power, 5);
+        }
+
+        void Update()
+        {
+            if (power && lerpTime < 1.5f)
+            {
+                power.transform.localScale = Vector3.Lerp(minScale, maxScale, lerpTime);
+                lerpTime += Time.deltaTime * speed;
+            }
+        }
+    }
+}
