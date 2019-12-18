aa61ee172504891f8fbd5d9f78dd567db6fcfd23
donkriegs <sonnm30@gmail.com>
2019-12-18 10:21:17 +0700

    enemy movement and attack script

---
+++ Assets/ARBrushGod/Scripts/Enemy/EnemyAttack.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    public float timeBetweenAttacks = 0.5f;
+    public int attackDamage;
+
+    Animator animator;
+    GameObject goal;
+    GoalHealth goalHealth;
+    EnemyHealth enemyHealth;
+    bool goalInRange;
+    float timer;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        goal = GameObject.FindGameObjectWithTag("Goal");
+        goalHealth = goal.GetComponent<GoalHealth>();
+        enemyHealth = GetComponent<EnemyHealth>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == goal)
+        {
+            animator.SetTrigger("Attack");
+            goalInRange = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == goal)
+        {
+            animator.SetTrigger("StopAttack");
+            goalInRange = false;
+        }
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= timeBetweenAttacks && goalInRange && enemyHealth.currentHealth > 0)
+        {
+            Attack();
+        }
+
+        if(goalHealth.currentHealth <= 0)
+        {
+            animator.SetTrigger("GoalDestroyed");
+        }
+    }
+
+    void Attack()
+    {
+        timer = 0f;
+        
+        if(goalHealth.currentHealth > 0)
+        {
+            goalHealth.TakeDamage(attackDamage);
+        }
+    }
+}
---
+++ Assets/ARBrushGod/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+public class EnemyMovement : MonoBehaviour
+{
+    Transform goal;               // Reference to the goal's position.
+    GoalHealth goalHealth;      // Reference to the goal's health.
+    EnemyHealth enemyHealth;        // Reference to this enemy's health.
+    NavMeshAgent nav;               // Reference to the nav mesh agent.
+
+
+    void Awake ()
+    {
+        // Set up the references.
+        goal = GameObject.FindGameObjectWithTag ("Goal").transform;
+        goalHealth = goal.GetComponent<GoalHealth>();
+        enemyHealth = GetComponent <EnemyHealth> ();
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+
+    void Update ()
+    {
+        // If the enemy and the goal have health left...
+        if(enemyHealth.currentHealth > 0 && goalHealth.currentHealth > 0)
+        {
+            // ... set the destination of the nav mesh agent to the goal.
+            nav.SetDestination (goal.position);
+        }
+        // Otherwise...
+        else
+        {
+            // ... disable the nav mesh agent.
+            nav.enabled = false;
+        }
+    } 
+}
