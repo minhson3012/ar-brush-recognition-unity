4e02578b0ce88ca882aba217976d1dd416cdc062
donkriegs <sonnm30@gmail.com>
2020-01-07 16:39:52 +0700

    update tree

--- Assets/ARBrushGod/Scripts/BrushPowers/TreeScript.cs
+++ Assets/ARBrushGod/Scripts/BrushPowers/TreeScript.cs
@@ -8,7 +8,7 @@ namespace BrushGestures
     public class TreeScript : MonoBehaviour
     {
         public GameObject Tree;
-        public float radius = 0.1f;
+        public float radius = 0.3f;
         public float speed = 0.5f;
         public float duration = 1.5f;
         public float inkCost = 3f;
@@ -18,27 +18,57 @@ namespace BrushGestures
         Vector3 minScale = new Vector3(0.001f, 0.001f, 0.001f);
         Vector3 maxScale = new Vector3(0.006f, 0.006f, 0.006f);
         float lerpTime = 0f;
+        GameObject[] enemies;
 
         public void SpawnTree()
         {
             brushPowers = GetComponent<BrushPowers>();
             circle = GameObject.FindGameObjectWithTag("Circle");
             Vector3 position = new Vector3(circle.transform.position.x, circle.transform.position.y, circle.transform.position.z);
+
+            //Create power
             power = Instantiate(Tree, position, Tree.transform.rotation);
             power.transform.parent = GameObject.FindGameObjectWithTag("Anchor").transform;
+
+            //Reduce Ink
             transform.GetComponent<InkScript>().ReduceInk(inkCost);
+
+            //Cleanup
             brushPowers.CleanupUI();
             Destroy(power, 5);
         }
 
         void Update()
         {
-            if (power && lerpTime < 1.5f)
+            if (power)
             {
-                power.transform.localScale = Vector3.Lerp(minScale, maxScale, lerpTime);
-                lerpTime += Time.deltaTime * speed;
+                if (lerpTime < 1.5f)
+                {
+                    power.transform.localScale = Vector3.Lerp(minScale, maxScale, lerpTime);
+                    lerpTime += Time.deltaTime * speed;
+                }
+                CheckForHit();
             }
             else if (!power) lerpTime = 0f;
+
+
+        }
+
+        private void CheckForHit()
+        {
+            if ((enemies = GameObject.FindGameObjectsWithTag("Enemy")) != null)
+            {
+                foreach (GameObject e in enemies)
+                {
+                    float distance = Vector3.Distance(power.transform.position, e.transform.position);
+                    if (distance <= radius)
+                    {
+                        //Stop enemies in tree range
+                        EnemyMovement em = e.GetComponent<EnemyMovement>();
+                        em.SetMoveTime(duration);
+                    }
+                }
+            }
         }
     }
 }
--- Assets/ARBrushGod/Scripts/Enemy/EnemyMovement.cs
+++ Assets/ARBrushGod/Scripts/Enemy/EnemyMovement.cs
@@ -1,85 +1,97 @@
 ﻿using UnityEngine;
 using UnityEngine.AI;
 using System.Collections;
-public class EnemyMovement : MonoBehaviour
+
+namespace BrushGestures
 {
-    public float rotationTime = 1f;
-    public float moveTime = 10f;
-    public float rainMultiplier = 5f;
-    public float windMultiplier = 10f;
-    Transform goal;               // Reference to the goal's position.
-    GoalHealth goalHealth;      // Reference to the goal's health.
-    EnemyHealth enemyHealth;        // Reference to this enemy's health.
-    EnemyAttack enemyAttack;
-    // NavMeshAgent nav;               // Reference to the nav mesh agent.
-
-    Quaternion lookRotation;
-    Vector3 direction;
-    float currentMoveTime;
-    float t = 0f;
-    bool isSlowed;
-
-    void Start()
+    public class EnemyMovement : MonoBehaviour
     {
-        // Set up the references.
-        goal = GameObject.FindGameObjectWithTag("Goal").transform;
-        goalHealth = goal.GetComponent<GoalHealth>();
-        enemyHealth = GetComponent<EnemyHealth>();
-        enemyAttack = GetComponent<EnemyAttack>();
-        currentMoveTime = moveTime;
-        StartCoroutine(MoveToPosition(transform, goal.position, currentMoveTime));
-        isSlowed = false;
-    }
+        public float rotationTime = 1f;
+        public float moveTime = 10f;
+        public float rainMultiplier = 5f;
+        public float windMultiplier = 10f;
+        public bool isNearTree = false;
+        Transform goal;               // Reference to the goal's position.
+        GoalHealth goalHealth;      // Reference to the goal's health.
+        EnemyHealth enemyHealth;        // Reference to this enemy's health.
+        EnemyAttack enemyAttack;
+        // NavMeshAgent nav;               // Reference to the nav mesh agent.
 
+        Quaternion lookRotation;
+        Vector3 direction;
+        float currentMoveTime;
+        float t = 0f;
+        bool isSlowed;
 
-    void Update()
-    {
-        if (enemyHealth.currentHealth > 0 && goalHealth.currentHealth > 0)
+        void Start()
         {
-            direction = (goal.position - transform.position).normalized;
-            lookRotation = Quaternion.LookRotation(direction);
-            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * rotationTime);
+            // Set up the references.
+            goal = GameObject.FindGameObjectWithTag("Goal").transform;
+            goalHealth = goal.GetComponent<GoalHealth>();
+            enemyHealth = GetComponent<EnemyHealth>();
+            enemyAttack = GetComponent<EnemyAttack>();
+            currentMoveTime = moveTime;
+            StartCoroutine(MoveToPosition(transform, goal.position, currentMoveTime));
+            isSlowed = false;
+            isNearTree = false;
+        }
 
-            if(transform.position.y != goal.position.y)
+
+        void Update()
+        {
+            if (enemyHealth.currentHealth > 0 && goalHealth.currentHealth > 0)
             {
-                Vector3 newPosition = new Vector3(transform.position.x, goal.position.y, transform.position.z);
-                transform.position = newPosition;
+
+                GameObject treeObject;
+                if ((treeObject = GameObject.FindGameObjectWithTag("Tree")) != null)
+                    direction = (treeObject.transform.position - transform.position).normalized;
+                else
+                    direction = (goal.position - transform.position).normalized;
+                lookRotation = Quaternion.LookRotation(direction);
+                transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * rotationTime);
+
+                if (transform.position.y != goal.position.y)
+                {
+                    Vector3 newPosition = new Vector3(transform.position.x, goal.position.y, transform.position.z);
+                    transform.position = newPosition;
+                }
+            }
+            else SetMoveTime(100f); //Stop moving when this enemy or the goal is dead
+        }
+
+        public IEnumerator MoveToPosition(Transform transform, Vector3 position, float moveTime)
+        {
+            var currentPos = transform.position;
+            t = 0f;
+            while (t < 1 && !enemyAttack.goalInRange)
+            {
+                yield return new WaitUntil(() => !isNearTree); // Don't move until tree is gone
+                t += Time.deltaTime / currentMoveTime;
+                transform.position = Vector3.Lerp(currentPos, position, t);
+                yield return null;
             }
         }
-        else SetMoveTime(100f, "normal"); //Stop moving when this enemy or the goal is dead
-    }
 
-    public IEnumerator MoveToPosition(Transform transform, Vector3 position, float moveTime)
-    {
-        var currentPos = transform.position;
-        t = 0f;
-        while (t < 1 && !enemyAttack.goalInRange)
+        public void SetMoveTime(float duration, string slowType = "default")
         {
-            t += Time.deltaTime / currentMoveTime;
-            transform.position = Vector3.Lerp(currentPos, position, t);
-            yield return null;
-        }
-    }
-
-    public void SetMoveTime(float duration, string slowType = "normal")
-    {
-        if (!isSlowed)
-        {
-            if(slowType == "rain") 
+            if (!isSlowed)
             {
-                currentMoveTime = currentMoveTime * rainMultiplier; //Different rain slow multiplier for different zombies
+                if (slowType == "rain")
+                {
+                    currentMoveTime = currentMoveTime * rainMultiplier; //Different rain slow multiplier for different zombies
+                }
+                else if (slowType == "wind") currentMoveTime = currentMoveTime * rainMultiplier;
+                else if (slowType == "default") currentMoveTime *= 100f;
+                isSlowed = true;
+                StartCoroutine(ResetMoveTime(duration));
             }
-            else if(slowType == "wind") currentMoveTime = currentMoveTime * rainMultiplier;
-            else if(slowType == "normal") currentMoveTime *= 100f;
-            isSlowed = true;
-            StartCoroutine(ResetMoveTime(duration));
         }
-    }
 
-    IEnumerator ResetMoveTime(float duration)
-    {
-        yield return new WaitForSeconds(duration);
-        currentMoveTime = moveTime;
-        isSlowed = false;
+        IEnumerator ResetMoveTime(float duration)
+        {
+            yield return new WaitForSeconds(duration);
+            currentMoveTime = moveTime;
+            isSlowed = false;
+        }
     }
 }
